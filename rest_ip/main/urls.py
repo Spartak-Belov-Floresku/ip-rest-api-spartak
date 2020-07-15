5239d2c90e96a82c2c984fd8041113caa1844ee1
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2020-07-15 01:32:12 -0400

    Update urls.py

--- rest_ip/main/urls.py
+++ rest_ip/main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework import routers
-from django.views.generic import TemplateView
 from main import endpoints
 
 router = routers.DefaultRouter()
